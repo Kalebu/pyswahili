d991c54971f81dcf36d2c0760ae98bf7a195cb3c
Kalebu Gwalugano <isaackeinstein@gmail.com>
2020-12-25 13:08:01 +0300

    added pyswahili repl

--- pyswahili/src/pyswahili.py
+++ pyswahili/src/pyswahili.py
@@ -6,16 +6,62 @@ from swahili_node import PySwahili
 
 
 translator = PySwahili()
+block_keywords = list(translator.sw_to_en['block_keywords'].values())
 console = code.InteractiveConsole()
 intepreter = code.InteractiveInterpreter()
+
+def remove_identation(line_of_code):
+    while True:
+        if line_of_code.startswith('\t'):
+            line_of_code = line_of_code[1:]
+            continue
+        break
+    return line_of_code
+
+
+def is_blocky(line_of_code):
+    return any([line_of_code.startswith(keyword) for keyword in block_keywords]) and line_of_code.endswith(':')
+
+def is_valid(line_of_code):
+    try:
+        line_of_code = remove_identation(line_of_code)
+        code.compile_command(line_of_code)
+        return True 
+    except Exception as bug:
+        print(bug)
+        return False
+
+def read():
+    try:
+        user_input = ""
+        command = input(">>> ")
+        english_command = translator.convert_to_english(command)
+        if is_valid(english_command):
+            user_input = english_command
+            if is_blocky(english_command):
+                while True:
+                    command = input("...")
+                    if command:
+                        english_command = translator.convert_to_english(command)
+                        if is_valid(english_command):        
+                            user_input = user_input + "\n" + english_command
+                            continue
+                    break
+            return user_input
+        return False
+
+    except Exception as bug:
+        print(bug)
 
 
 def repl():
     while True:
         try:
-            swahili_command = input('>>> ')
-            english_command = translator.convert_to_english(swahili_command)
-            repr(console.runcode(english_command))
+            command = read()
+            if command:
+                repr(console.runcode(command))
+                continue
+            print('umeokosea sarufi')
         except KeyboardInterrupt:
             pass
         else:
--- pyswahili/src/swahili_node.py
+++ pyswahili/src/swahili_node.py
@@ -4,18 +4,15 @@ import json
 class PySwahili(object):
     def __init__(self):
         arguments = list(sys.argv)
-        print(arguments)
         if len(arguments)>1:
             self.swahili_code = sys.argv[1]
         self.sw_to_en = self.load_keyword_dictionary()
-        print(self.sw_to_en)
 
 
     @staticmethod
     def load_keyword_dictionary():
         try:
             with open('sw_to_english.json') as dictionary:
-                print('loading dictionary')
                 return json.load(dictionary)
         except FileNotFoundError:
             print('Make sure json keyword dictionary is on your current directory')
@@ -36,10 +33,11 @@ class PySwahili(object):
 
     
     def convert_to_english(self, sw_python_code):
-        sw_keywords = list(self.sw_to_en.keys())
+        sw_to_en = self.sw_to_en['keywords']
+        sw_keywords = list(sw_to_en.keys())
         for sw_keyword in sw_keywords:
             if sw_keyword in sw_python_code:
-                sw_python_code = sw_python_code.replace(sw_keyword, self.sw_to_en[sw_keyword])
+                sw_python_code = sw_python_code.replace(sw_keyword, sw_to_en[sw_keyword])
         return sw_python_code
 
 
