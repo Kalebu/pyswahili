eb07748e613c967b0f2fab421e2af068695ff5e8
Kalebu Gwalugano <isaackeinstein@gmail.com>
2020-12-25 13:20:36 +0300

    restuctured python-reprl as a class

--- hello.py
+++
@@ -1,7 +0,0 @@
-umri = ingiza('Umri wako: ')
-umri = int(umri)
-
-kama umri < 18:
-  andika('bado kijana mdogo')
-zaidi:
-  andika('sasa waweza date')
--- pyswahili/src/pyswahili.py
+++ pyswahili/src/pyswahili.py
@@ -5,67 +5,70 @@ import click
 from swahili_node import PySwahili
 
 
-translator = PySwahili()
-block_keywords = list(translator.sw_to_en['block_keywords'].values())
-console = code.InteractiveConsole()
-intepreter = code.InteractiveInterpreter()
 
-def remove_identation(line_of_code):
-    while True:
-        if line_of_code.startswith('\t'):
-            line_of_code = line_of_code[1:]
-            continue
-        break
-    return line_of_code
+class PySwahili_Repl:
+    def __init__(self):
+        self.translator = PySwahili()
+        self.block_keywords = list(self.translator.sw_to_en['block_keywords'].values())
+        self.console = code.InteractiveConsole()
+        self.intepreter = code.InteractiveInterpreter()
 
-
-def is_blocky(line_of_code):
-    return any([line_of_code.startswith(keyword) for keyword in block_keywords]) and line_of_code.endswith(':')
-
-def is_valid(line_of_code):
-    try:
-        line_of_code = remove_identation(line_of_code)
-        code.compile_command(line_of_code)
-        return True 
-    except Exception as bug:
-        print(bug)
-        return False
-
-def read():
-    try:
-        user_input = ""
-        command = input(">>> ")
-        english_command = translator.convert_to_english(command)
-        if is_valid(english_command):
-            user_input = english_command
-            if is_blocky(english_command):
-                while True:
-                    command = input("...")
-                    if command:
-                        english_command = translator.convert_to_english(command)
-                        if is_valid(english_command):        
-                            user_input = user_input + "\n" + english_command
-                            continue
-                    break
-            return user_input
-        return False
-
-    except Exception as bug:
-        print(bug)
-
-
-def repl():
-    while True:
-        try:
-            command = read()
-            if command:
-                repr(console.runcode(command))
+    @staticmethod
+    def remove_identation(line_of_code):
+        while True:
+            if line_of_code.startswith('\t'):
+                line_of_code = line_of_code[1:]
                 continue
-            print('umeokosea sarufi')
-        except KeyboardInterrupt:
-            pass
-        else:
-            continue
+            break
+        return line_of_code
+
+    def is_blocky(self, line_of_code):
+        return any([line_of_code.startswith(keyword) for keyword in self.block_keywords]) and line_of_code.endswith(':')
+
+    def is_valid(self, line_of_code):
+        try:
+            line_of_code = self.remove_identation(line_of_code)
+            code.compile_command(line_of_code)
+            return True 
+        except Exception as bug:
+            print(bug)
+            return False
+
+    def read_user_input(self):
+        try:
+            user_input = ""
+            command = input(">>> ")
+            english_command = self.translator.convert_to_english(command)
+            if self.is_valid(english_command):
+                user_input = english_command
+                if self.is_blocky(english_command):
+                    while True:
+                        command = input("...")
+                        if command:
+                            english_command = self.translator.convert_to_english(command)
+                            if self.is_valid(english_command):        
+                                user_input = user_input + "\n" + english_command
+                                continue
+                        break
+                return user_input
+            return False
+
+        except Exception as bug:
+            print(bug)
+
+
+    def repl(self):
+        print('Pyswahili 0.0.1 by @kalebujordan')
+        while True:
+            try:
+                command = self.read_user_input()
+                if command:
+                    repr(self.console.runcode(command))
+                    continue
+            except KeyboardInterrupt:
+                sys.exit()
+            else:
+                continue
 
 if __name__ == '__main__':
-    repl()
+    PySwahili_Repl().repl()
