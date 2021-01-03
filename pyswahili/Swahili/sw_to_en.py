9d8e3f542d88a35ecb95c81e799081b790ba26e0
Kalebu Gwalugano <isaackeinstein@gmail.com>
2021-01-03 08:01:23 +0300

    changed vinginevyo to zaidi

--- pyswahili/Swahili/sw_to_en.py
+++ pyswahili/Swahili/sw_to_en.py
@@ -1,44 +1,43 @@
-dictionary = { "keywords": {
-                "tupu": "None",
-                "Kweli": "True",
-                "SiKweli": "False",     #semantical correction
-                "vunja": "break",
-                "endelea": "continue",
-                "rudisha": "return",
-                "ingiza": "input",
-                "kama":"if",
-                "pia": "elif",          #as long as it makes sense semantical translation does not matter
-                "ikiwa": "for",
-                "katiya":"range",
-                "imo": "in",
-                "wakati": "while",
-                "andika":"print",
-                "vinginevyo": "else",
-                "njia": "def",          #njia is a more relevant keyword to functions/method
-                "pamoja" : "with",
-                "darasa": "class", 
-                "futa": "del",          #futa is a lot simple to catch with and is commonly used than ondoa 
-                "kutoka":"from", 
-                "sio":"not", 
-                "ni": "is",
-                "au": "or",
-                "na": "and", 
-                "neno":"str", 
-                "boksi":"list", 
-                "kamusi":"dict", 
-                "jaribu":"try", 
-                "ila":"except"          #ila is a lot simple to catch with and most relevant translation of except
-    }, 
-
+dictionary = {
+    "keywords": {
+        "tupu": "None",
+        "Kweli": "True",
+        "SiKweli": "False",  # semantical correction
+        "vunja": "break",
+        "endelea": "continue",
+        "rudisha": "return",
+        "ingiza": "input",
+        "kama": "if",
+        "pia": "elif",  # as long as it makes sense semantical translation does not matter
+        "ikiwa": "for",
+        "katiya": "range",
+        "imo": "in",
+        "wakati": "while",
+        "andika": "print",
+        "zaidi": "else",
+        "njia": "def",  # njia is a more relevant keyword to functions/method
+        "pamoja": "with",
+        "darasa": "class",
+        "futa": "del",  # futa is a lot simple to catch with and is commonly used than ondoa
+        "kutoka": "from",
+        "sio": "not",
+        "ni": "is",
+        "au": "or",
+        "na": "and",
+        "neno": "str",
+        "boksi": "list",
+        "kamusi": "dict",
+        "jaribu": "try",
+        "ila": "except",  # ila is a lot simple to catch with and most relevant translation of except
+    },
     "block_keywords": {
-      "kama":"if", 
-      "pia": "elif",                    #reference line 10
-      "vinginevyo":"else", 
-      "wakati":"while",
-      "ikiwa":"for", 
-      "njia": "def",                    #reference line 17
-      "pamoja":"with",
-      "ila":"except"                    #reference line 30
-    }
-
+        "kama": "if",
+        "pia": "elif",  # reference line 10
+        "vinginevyo": "else",
+        "wakati": "while",
+        "ikiwa": "for",
+        "njia": "def",  # reference line 17
+        "pamoja": "with",
+        "ila": "except",  # reference line 30
+    },
 }
