fb0a1105c998357d3127cbf508272b0bcf96f1c9
Kalebu <isaackeinstein@gmail.com>
2021-04-19 23:37:39 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,15 @@ from setuptools import setup
 setup(
     name="pyswahili",
     version="0.1.0",
+    description="""
+    Python package for briding python english keywords 
+    with swahili one to allow swahili speakers to learn the basics of coding 
+    without ever knowing  english 
+    """,
+    url="https://github.com/Kalebu/pyswahili",
+    author="Jordan Kalebu",
+    author_email="isaackeinstein@gmail.com",
+    license="MIT",
     packages=["pyswahili"],
     entry_points={
         "console_scripts": [
