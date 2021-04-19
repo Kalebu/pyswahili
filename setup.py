e6a376d6f678a451d7cd80ecf20c22cef3bc4bbd
Kalebu <isaackeinstein@gmail.com>
2021-04-19 23:44:14 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -9,13 +9,36 @@ setup(
     without ever knowing  english 
     """,
     url="https://github.com/Kalebu/pyswahili",
+    download_url='https://github.com/Kalebu/pyswahili/releases/tag/0.1',
     author="Jordan Kalebu",
     author_email="isaackeinstein@gmail.com",
     license="MIT",
     packages=["pyswahili"],
+    keywords=[
+        "pyswahili",
+        "python-tanzania",
+        "python-transpiler",
+        "swahili-python",
+        "python in swahili",
+        "python for swahili speakers",
+        "code python in swahili",
+        "swahili programming language",
+        "program in swahili",
+    ],
     entry_points={
         "console_scripts": [
             "pyswahili = pyswahili.__main__:main"
         ]
     },
+    python_requires=">=3.6",
+    classifiers=[
+        "Development Status :: 1 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ]
 )
